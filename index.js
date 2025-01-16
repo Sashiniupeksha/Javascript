d817d3e8063c9884aeb8f77be0b1695abd0383b7
Sashiniupeksha <121797693+Sashiniupeksha@users.noreply.github.com>
2025-01-16 13:46:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,4 +21,16 @@ document.getElementById("myH1").textContent =`Hello`;
 document.getElementById("myP").textContent = `I like Pizza`;
 
 
+variable
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+
+console.log(`you are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your gpa is: ${gpa}`);
+
+
   
