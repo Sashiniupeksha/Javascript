eb38950e86bd7f82ef9a086a41eea631e7f4a67d
Sashiniupeksha <121797693+Sashiniupeksha@users.noreply.github.com>
2025-01-16 13:08:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log(`Hello`);
+console.log(`I like pizza!`);
+
+
+window.alert(`This is an alert!`);
+window.alert(`I like Pizza!`);
+
+// This is a comment
+
+/* 
+  This 
+  is 
+  a 
+  comment
+  */
+
+  
